00f07951034c156070ea9dfe7619fdf05ed43c2a
ParviziAH <simone.parvizi@ah.nl>
2024-12-07 18:19:00 +0100

    updated sha256

--- git-ah.rb
+++ git-ah.rb
@@ -2,7 +2,7 @@ class GitAh < Formula
   desc "Git if it was made by Albert Heijn"
   homepage "https://github.com/parviziAH/git-ah"
   url "https://github.com/parviziAH/git-ah/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "841bf5348f0c147683a4a6aaa974e35ecd960a70087221082b66caa17b8d54b1"
   license "MIT"
 
   depends_on "python@3.9" # Or the version of Python your script requires
